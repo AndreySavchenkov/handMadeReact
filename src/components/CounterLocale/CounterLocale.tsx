5cfb58d0fe457232e7df84cac4a49a8b502ee2aa
sandvik16 <sannndvik16@gmail.com>
2021-08-30 22:06:39 +0300

    work with localstorage in React with useEffect

--- src/components/CounterLocale/CounterLocale.tsx
+++ src/components/CounterLocale/CounterLocale.tsx
@@ -1,35 +1,24 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
 export const CounterLocale = () => {
 
     const [value, setValue] = useState(0)
 
-    const incHandler = () => {
-        setValue(value + 1)
-    }
-
-    const setToLocalstorageHandler = () => {
-        localStorage.setItem('counterValue', JSON.stringify(value))
-        localStorage.setItem('counterValue + 1', JSON.stringify(value))
-    }
-
-    const getFromLocalstorageHandler = () => {
+    useEffect(()=> {
         let valueAsString = localStorage.getItem('counterValue')
         if(valueAsString) {
             let newValue = JSON.parse(valueAsString);
             setValue(newValue)
         }
+    }, [])
 
-    }
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }, [value])
 
-    const clearLocalstorageHandler = () => {
-        localStorage.clear()
-        setValue(0)
-    }
-
-    const removeItemLocalstorageHandler = () => {
-        localStorage.removeItem('counterValue')
+    const incHandler = () => {
+        setValue(value + 1)
     }
 
 
@@ -39,10 +28,6 @@ export const CounterLocale = () => {
         <div>
             <h1>{value}</h1>
             <button onClick={incHandler}>inc</button>
-            <button onClick={setToLocalstorageHandler}>setToLocalstorage</button>
-            <button onClick={getFromLocalstorageHandler}>getFromLocalstorage</button>
-            <button onClick={clearLocalstorageHandler}>clearLocalstorage</button>
-            <button onClick={removeItemLocalstorageHandler}>removeItemLocalstorage</button>
         </div>
     )
 }
